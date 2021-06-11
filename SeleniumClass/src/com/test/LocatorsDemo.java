a564673a9559965cfd82b734d6a87aac5d575b15
PrasanthAbbos <10prasanth1994@gmail.com>
2021-06-11 09:36:22 +0530

    udated the xpath

--- SeleniumClass/src/com/test/LocatorsDemo.java
+++ SeleniumClass/src/com/test/LocatorsDemo.java
@@ -1,43 +1,50 @@
 package com.test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LocatorsDemo {
 
 	//Locators
 	//We have 8 Locators
-	
 	//1.using id = id="src"
 	//2.class class="db"
 	//3.name - name = 
 	//4.linkText
 	//5.PartialLinkText
 	//6.tagname - a	
-	
 	//7.xpath
 	//8.cssselector
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "G:\\Selenium Drivers\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
 		driver.get("https://demoqa.com/automation-practice-form");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
 			
 		WebElement from = driver.findElement(By.id("firstName"));
 		from.sendKeys("Victor");
-		WebElement last = driver.findElement(By.id("lastName"));
-		last.sendKeys("Sam");
+		//WebElement last = driver.findElement(By.id("lastName"));
+		//last.sendKeys("Sam");
 		
 		List<WebElement> inputs = driver.findElements(By.tagName("a"));
 		
 		System.out.println(inputs.size());
 			
 		driver.get("https://www.google.com/");
+		
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().to("https://www.google.com/");
+		driver.navigate().refresh();
 		/*driver.findElement(By.name("q")).sendKeys("Selenium");
 		driver.findElement(By.name("btnK")).click();*/
 		//driver.findElement(By.linkText("About")).click();
---
+++ SeleniumClass/src/com/test/XpathDemo.java
@@ -0,0 +1,63 @@
+package com.test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathDemo {
+
+	public static void main(String[] args) {
+		/*//xpath - dynamic locator and travers
+		//1.absoulte 2.relativ
+		//absoulte -  direct way 
+		'/' - /html/body/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[2]/div[1]/div/button
+		
+		relative- middle location 
+		'//' - //*[@id="menu1"]
+*/	
+		/*Basic xpath
+		//tagname[@attri='arrtival']  - //button[@type='button']
+		
+		Contains
+		//p[contains(text(),'The .dropdown class is used to')]
+		//div[contains(@class,'container')]
+	
+		logical  - OR and AND
+		//ul[@class='dropdown-menu' and @role='menu1']
+		//ul[@class='dropdown-menu' or @role='menu1']
+		*/
+		
+		System.setProperty("webdriver.chrome.driver", "G:\\Selenium Drivers\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/automation-practice-form");
+		
+		
+		driver.findElement(By.xpath("//input[contains(@placeholder,'First Name')]")).sendKeys("Test");
+		//*[@id="firstName"]
+		driver.findElement(By.xpath("//input[@type='text' and @id='lastName']")).sendKeys("last test");
+	/*	driver.findElement(By.xpath("//label[contains(text(),'Male')]")).click();
+		
+		String gender = driver.findElement(By.xpath("//label[contains(text(),'Male')]")).getText();
+		
+		
+		System.out.println(gender);*/
+		
+		
+		List<WebElement> genders = driver.findElements(By.xpath("//div[@id='genterWrapper']//div[@class='col-md-9 col-sm-12']//div//label"));
+		
+		for(int i=0;i<genders.size()-1;i++)
+		{
+			if(genders.get(i).getText().equals("Female")) {
+				genders.get(i).click();
+				break;
+			}
+		
+		}
+		}
+	
+	
+
+}
